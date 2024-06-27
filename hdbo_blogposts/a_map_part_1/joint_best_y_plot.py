a4cd8951ac54167ee7a663f1b796bf522aef16fb
Miguel González Duque <miguelgondu@gmail.com>
2024-06-27 21:53:57 +0200

    Updates the joint best plot to use local paths

--- hdbo_blogposts/a_map_part_1/joint_best_y_plot.py
+++ hdbo_blogposts/a_map_part_1/joint_best_y_plot.py
@@ -1,8 +1,11 @@
+from pathlib import Path
 import json
 
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+THIS_DIR = Path(__file__).parent.resolve()
 
 sns.set_theme(style="darkgrid", font_scale=2.0)
 
@@ -20,7 +23,7 @@ NAMES_BUT_PRETTY = {
 
 best_ys = {}
 for solver_name in SOLVER_NAMES:
-    with open(f"{solver_name}_history.json", "r") as f:
+    with open(THIS_DIR / f"{solver_name}_history.json", "r") as f:
         best_y = np.array(json.load(f)["best_y"]).flatten()
 
     best_ys[solver_name] = best_y
@@ -37,7 +40,9 @@ ax.set_xlabel("Iteration")
 ax.set_ylabel("Best value so far")
 fig.tight_layout()
 fig.savefig(
-    f"joint_best_y_plot_{'_'.join(SOLVER_NAMES)}.jpg", dpi=300, bbox_inches="tight"
+    THIS_DIR / f"joint_best_y_plot_{'_'.join(SOLVER_NAMES)}.jpg",
+    dpi=300,
+    bbox_inches="tight",
 )
 
 plt.show()
